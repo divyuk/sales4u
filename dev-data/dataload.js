7109862474d57c83a4394d663bcdb04486eae940
divyuk <div14kes@gmail.com>
2023-07-19 21:14:44 +0530

    eslint and prettier condigured

--- dev-data/dataload.js
+++ dev-data/dataload.js
@@ -1,11 +1,12 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const dotenv = require("dotenv");
-dotenv.config({ path: "config.env" });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: 'config.env' });
 
 class Loader {
   constructor() {
     this.DB = process.env.DATABASE_CONNECTION_STRING.replace(
-      "<PASSWORD>",
+      '<PASSWORD>',
       process.env.DATABASE_PASSWORD
     );
     this.client = null;
@@ -13,13 +14,14 @@ class Loader {
     this.collection = process.env.DATABASE_COLLECTION;
     this.dbcollection = null;
   }
+
   async fetchData() {
     try {
-      const respose = await fetch("https://dummyjson.com/products");
+      const respose = await fetch('https://dummyjson.com/products');
       const data = await respose.json();
-      return data["products"];
+      return data.products;
     } catch (err) {
-      console.log("Error in loading the file");
+      console.log('Error in loading the file');
       throw err;
     }
   }
@@ -36,29 +38,31 @@ class Loader {
       await this.client.connect();
       const myDB = await this.client.db(this.dbname);
       this.dbcollection = await myDB.collection(this.collection);
-      console.log("Connected to MongoDB successfully");
+      console.log('Connected to MongoDB successfully');
     } catch (err) {
-      console.error("Error connecting to MongoDB:", err);
+      console.error('Error connecting to MongoDB:', err);
       throw err;
     }
   }
+
   async insertData(data) {
     try {
       await this.connectToDB();
       await this.dbcollection.insertMany(data);
-      console.log("Successfully Inserted");
+      console.log('Successfully Inserted');
     } catch (error) {
-      console.log("Error", error);
+      console.log('Error', error);
     } finally {
       // Ensures that the client will close when you finish/error
       await this.client.close();
     }
   }
+
   async deleteAll() {
     try {
       await this.connectToDB();
       const deleteManyResult = await this.dbcollection.drop();
-      console.log("Successfully Deleted", deleteManyResult);
+      console.log('Successfully Deleted', deleteManyResult);
     } finally {
       // Ensures that the client will close when you finish/error
       await this.client.close();
@@ -72,6 +76,6 @@ const loader = new Loader();
     await loader.insertData(data);
     // await loader.deleteAll(data);
   } catch (error) {
-    console.log("Cant Load", error);
+    console.log('Cant Load', error);
   }
 })();
